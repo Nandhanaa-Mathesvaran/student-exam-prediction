e11ba9c72c01877cfcf3138001499dd7ed0194e6
Nandhanaa-Mathesvaran <nandhanaamathesvaran@gmail.com>
2025-10-12 10:03:54 +0530

    first

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import pickle
 model = pickle.load(open("trained_model.sav", "rb"))
 
 st.title("Student Math Score Prediction App")
-
 st.write("Predict a student's Math Score based on their attributes.")
 
 reading = st.number_input("Reading Score", min_value=0, max_value=100, value=70)
@@ -15,7 +14,14 @@ lunch = st.selectbox("Lunch Type", ["standard", "free/reduced"])
 test_prep = st.selectbox("Test Preparation Course", ["completed", "none"])
 parent_edu = st.selectbox(
     "Parental Level of Education",
-    ["some high school", "high school", "associate's degree", "bachelor's degree", "master's degree"]
+    [
+        "some high school",
+        "high school",
+        "associate's degree",
+        "some college",
+        "bachelor's degree",
+        "master's degree"
+    ]
 )
 
 lunch_num = 1 if lunch == "standard" else 0
@@ -25,8 +31,9 @@ parent_edu_map = {
     "some high school": 1,
     "high school": 2,
     "associate's degree": 3,
-    "bachelor's degree": 4,
-    "master's degree": 5
+    "some college": 4,
+    "bachelor's degree": 5,
+    "master's degree": 6
 }
 parent_edu_num = parent_edu_map[parent_edu]
 
